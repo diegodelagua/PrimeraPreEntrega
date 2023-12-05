c06ed4bbce13b855298e71c392afd346d9f6657b
Diego Del Agua <diego.delagua@hotmail.com>
2023-12-04 21:26:32 -0300

    fixeo

--- JS/javascritp.js
+++ JS/javascritp.js
@@ -6,19 +6,19 @@ let precio = 0, cuotas = 0, pc = 0, continuar = "no";
 function calcularTotal (precio, cuotas){
     switch (cuotas) {
         //en 3 cuotas tiene un recargo del 10%
-        case 3: pc = (precio + precio * 10/100) /3;
+        case 3: pc = ((precio + precio * 10/100) /3).toFixed(2);
             alert (`El precio final seria de $${pc*3} en 3 cuotas de $${pc}`)
             break;
         //en 6 cuotas tiene un recargo del 20%
-        case 6: pc = (precio + precio * 35/100) /6;
+        case 6: pc = ((precio + precio * 35/100) /6).toFixed(2);
             alert (`El precio final seria de $${pc*6} en 6 cuotas de $${pc}`)
             break;
         //en 9 cuotas tiene un recargo del 75%    
-        case 9: pc = (precio + precio * 75/100) /9;
+        case 9: pc = ((precio + precio * 75/100) /9).toFixed(2);
             alert (`El precio final seria de $${pc*9} en 9 cuotas de $${pc}`)
             break;
         //en 12 cuotas tiene un recargo del 125%    
-        case 12: pc = (precio + precio * 125/100) /12;
+        case 12: pc = ((precio + precio * 125/100) /12).toFixed(2);
             alert (`El precio final seria de $${pc*12} en 12 cuotas de $${pc}`)
             break;
     
@@ -31,7 +31,7 @@ function calcularTotal (precio, cuotas){
 }
 do {
     precio = parseFloat (prompt("Ingrese precio del producto"));
-    cuotas = parseInt (prompt("Ingrese numero cuotas (3,6,9,12)"));
+    cuotas = parseInt(prompt("Ingrese numero cuotas (3,6,9,12)"));
     calcularTotal (precio,cuotas);
     continuar = prompt("Desea continuar? (si/no)").toLowerCase();
 }
